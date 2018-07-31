74a08de0cb7d1530e9b6a03bf36204fef45d4244
Gerard del Castillo <gerarddelcastillo@gmail.com>
2018-07-31 14:23:43 +0200

    register models in admin

--- hardware_lab/admin.py
+++ hardware_lab/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
+from hardware_lab import models
+
+class HardwareTypeAdmin(admin.ModelAdmin):
+	def image_tag(self, obj):
+		return format_html('<img src="{}" />'.format(obj.image.url))
+
+	image_tag.short_description = 'Image'
+
+	list_display = ['name', 'image_tag']
+
+class HardwareItemAdmin(admin.ModelAdmin):
+	list_display = ['label', 'hardware_type', 'comments']
+
+
+admin.site.register(models.HardwareType, HardwareTypeAdmin)
+admin.site.register(models.HardwareItem, HardwareItemAdmin)
+admin.site.register(models.LogMessage)
