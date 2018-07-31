df312944e58132d73430541b9e713e9d5ff8d659
Gerard del Castillo <gerarddelcastillo@gmail.com>
2018-07-31 14:22:47 +0200

    add hardware type model and string casts

--- hardware_lab/models.py
+++ hardware_lab/models.py
@@ -17,16 +17,43 @@ STATUS = [
     (HW_UNAVAILABLE, 'Unavailable'),
 ]
 
+class HardwareType(models.Model):
+    #Human readable name
+    name = models.CharField(max_length=50)
+    #Image of the hardware
+    image = models.ImageField(upload_to='hw_images/')
+    #Description of this hardware 
+    #what is it used for? which items are contained in the package?
+    description = models.TextField()
+
+    def get_available_count(self):
+        return HardwareItem.objects.filter(status=HW_AVAILABLE).count()
+
+    def get_requested_count(self):
+        return HardwareItem.objects.filter(status=HW_REQUESTED).count()
+
+    def get_lent_count(self):
+        return HardwareItem.objects.filter(status=HW_LENT).count()
+
+    def get_unavailable_count(self):
+        return HardwareItem.objects.filter(status=HW_UNAVAILABLE).count()
+
+    def __str__(self):
+        return self.name
+
 class HardwareItem(models.Model):
+    #Hardware model/type
+    hardware_type = models.ForeignKey(HardwareType)
     #Identifies a real world object
     label = models.CharField(max_length=20)
-    #Human readable name
-    label = models.CharField(max_length=40)
     #Status of this item
     status = models.CharField(choices=STATUS, default=HW_AVAILABLE,
                               max_length=1)
     #Any other relevant information about this item
     comments = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} ({})'.format(self.label, self.hardware_type.name)
 
 class LogMessage(models.Model):
     user = models.ForeignKey(User)
