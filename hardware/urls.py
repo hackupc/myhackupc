3a239187afdbbd845846c77efcc93b03153ec780
Gerard del Castillo <gerarddelcastillo@gmail.com>
2018-08-01 14:47:21 +0200

    hardware admin view

--- hardware/urls.py
+++ hardware/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^list/$', views.HardwareListView.as_view(), name='hw_list'),
+    url(r'^admin/$', views.HardwareAdminView.as_view(), name='hw_admin'),
     url(r'^log/$', views.HardwareListView.as_view(), name='hw_log')
 ]
--- hardware/views.py
+++ hardware/views.py
@@ -2,17 +2,37 @@ from django.urls import reverse
 from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from user.mixins import IsVolunteerMixin
 from app.mixins import TabsViewMixin
 from hardware.models import ItemType
 
+def hardware_tabs(user):
+    first_tab = ('Hardware List', reverse('hw_list'), False)
+    if user.is_volunteer:
+        first_tab = ('Hardware Admin', reverse('hw_admin'), False)
+    return [
+        first_tab,
+        ('Log', reverse('hw_log'), False)
+    ]
+
 class HardwareListView(LoginRequiredMixin, TabsViewMixin, TemplateView):
-	template_name = 'hardware_list.html'
-	def get_current_tabs(self):
-		return [
-			('Hardware List', reverse('hw_list'), False),
-			('Log', reverse('hw_log'), False)
-		]
-	def get_context_data(self, **kwargs):
-		context = super(HardwareListView, self).get_context_data(**kwargs)
-		context['hw_list'] = ItemType.objects.all()
-		return context
+    template_name = 'hardware_list.html'
+
+    def get_current_tabs(self):
+        return hardware_tabs(self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(HardwareListView, self).get_context_data(**kwargs)
+        context['hw_list'] = ItemType.objects.all()
+        return context
+
+class HardwareAdminView(IsVolunteerMixin, TabsViewMixin, TemplateView):
+    template_name = 'hardware_admin.html'
+
+    def get_current_tabs(self):
+        return hardware_tabs(self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(HardwareAdminView, self).get_context_data(**kwargs)
+        context['hw_list'] = ItemType.objects.all()
+        return context
