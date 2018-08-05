ba7ad92cc8d43e13b2d793f8e9eb3bd313f3dc6d
Gerard del Castillo <gerarddelcastillo@gmail.com>
2018-08-06 01:24:29 +0200

    move scripts to static folder +refactor

---
+++ hardware/static/js/hw_admin.js
@@ -0,0 +1,59 @@
+
+let hw_admin = ((hw)=>{
+    if(!hw){
+        console.error("hw.js has to be declared before hw_admin.js")
+        return;  
+    } 
+    let obj = {};
+    //-Changes the content if any
+    //-Shows a toast if there's a message
+    obj.processResponse = (data)=>{
+        if(data.msg) 
+            hw.toast(data.msg)
+        
+        if(data.content){
+            $('#hw_container').hide(200, ()=>{
+                $('#hw_container').html(data.content)
+                obj.initListeners()
+                $('#hw_container').show(200)
+            })
+        }
+    }
+
+    obj.initListeners = ()=>{
+        $("#hw-user-send").on("click", ()=>{
+            hw.ajax_req({
+                'get_lists': true,
+                'email': $("#id_email").val()
+            }, hw.processResponse)
+        })
+        $("#hw-requests-list li").on("click", (ev)=>{
+            hw.ajax_req({
+                'select_request': true,
+                'request_id': ev.currentTarget.dataset.requestId
+            }, hw.processResponse)
+        })
+        $("#hw-lendings-list li").on("click", (ev)=>{
+            hw.ajax_req({
+                'return_item': true,
+                'lending_id': ev.currentTarget.dataset.lendingId
+            }, hw.processResponse)
+        })
+        $("#hw-available-items-list li").on("click", (ev)=>{
+            hw.ajax_req({
+                'make_lending': true,
+                'item_id': ev.currentTarget.dataset.itemId,
+                'request_id': ev.currentTarget.parentNode.dataset.requestId
+            }, hw.processResponse)
+        })
+    }
+   
+
+    
+    return obj
+})(hw)
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    hw_admin.initListeners()
+})
---
+++ hardware/static/js/hw_list.js
@@ -0,0 +1,22 @@
+let hw_list = ((hw)=>{
+    if(!hw){
+        console.error("hw.js has to be declared before hw_list.js")
+        return;  
+    } 
+    let obj = {}
+    obj.initListeners = ()=>{
+        $(".hw-req-btn").on("click", (ev)=>{
+            hw.ajax_req({
+                	'req_item':true, 
+                    'item_id': ev.target.dataset.itemId,
+            }, (data)=>{
+                if(data.msg) hw.toast(data.msg)
+                obj.inistListeners()
+            })
+        })
+    }
+})(hw)
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    hw_list.initListeners()
+})
