6dc67072b1324f1c5f2e0ca8098cb3fdc60a1b85
jmiarons <joelmiarons@gmail.com>
2024-03-04 09:19:47 +0100

    Remove unused import

--- reimbursement/models.py
+++ reimbursement/models.py
@@ -1,8 +1,6 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
-from datetime import timedelta
-
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
