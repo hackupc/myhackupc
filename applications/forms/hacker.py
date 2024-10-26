ef2c588911e430c5d404bbc81d10361ad9ca66cd
EncryptEx <41539618+EncryptEx@users.noreply.github.com>
2024-10-26 16:44:58 +0200

    fix: added placeholder at projects and changed help text

--- applications/forms/hacker.py
+++ applications/forms/hacker.py
@@ -209,8 +209,8 @@ class HackerApplicationForm(_BaseApplicationForm):
             "other_diet": "Please fill here in your dietary requirements. We want to make sure we have food for you!",
             "lennyface": 'tip: you can chose from here <a href="http://textsmili.es/" target="_blank">'
             " http://textsmili.es/</a>",
-            "projects": "You can talk about about past hackathons, personal projects, awards etc. "
-            "(we love links) Show us your passion! :D",
+            "projects": 
+            "Tell us about your personal projects, awards, or any work that you are proud of.   <br>",
             "reimb_amount": "We try our best to cover costs for all hackers, but our budget is limited",
             "resume": "Accepted file formats: %s"
             % (", ".join(extensions) if extensions else "Any"),
@@ -249,7 +249,7 @@ class HackerApplicationForm(_BaseApplicationForm):
         widgets = {
             "origin": forms.TextInput(attrs={"autocomplete": "off"}),
             "description": forms.Textarea(attrs={"rows": 3, "cols": 40}),
-            "projects": forms.Textarea(attrs={"rows": 3, "cols": 40}),
+            "projects": forms.Textarea(attrs={"rows": 3, "cols": 40, 'id': 'projects', 'placeholder': 'Developed a Google Maps feature for optimized routing based on traffic data,\nCreated a Discord bot for automating daily tasks,\nWon 2nd place in Hackathon for a mobile app that recommends eco-friendly travel routes.'}),
             "discover": CustomSelect(choices=discover_choices),
             "graduation_year": forms.RadioSelect(),
         }
