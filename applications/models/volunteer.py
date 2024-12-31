490f3260296104aed4f69dcf2c7ee788ca370eea
AwaKT <gerard.madrid@hackupc.com>
2024-12-31 11:16:51 +0100

    Removed hobbies from application

--- applications/models/volunteer.py
+++ applications/models/volunteer.py
@@ -102,7 +102,6 @@ class VolunteerApplication(BaseApplication):
     friends = models.CharField(max_length=100, null=True, blank=True)
     pronouns = models.CharField(max_length=100, null=True, blank=True)
     night_shifts = MultiSelectField(choices=NIGHT_SHIFT_ES, default='No')
-    hobbies = models.CharField(max_length=150, null=False)
     volunteer_motivation = models.CharField(max_length=500)
     valid = models.BooleanField(default=True)
 
