a0c59824632cf76801900d6c576eb0a47d904978
AdriMM26 <adria.martinez@hackupc.com>
2025-08-04 21:14:30 +0200

    Flake8 check

--- teams/models.py
+++ teams/models.py
@@ -1,10 +1,11 @@
 import random
 import string
-from django.db import models, IntegrityError, transaction
+from django.db import models
 from user.models import User
 
 TEAM_ID_LENGTH = 13
 MAX_ATTEMPTS = 33
+
 
 def generate_team_id():
     s = string.ascii_letters + string.digits  # 62 caràcters únics
@@ -13,6 +14,7 @@ def generate_team_id():
         if not Team.objects.filter(team_code=team_id).exists():
             return team_id
     raise Exception("No s'ha pogut generar un team_code únic després de diversos intents.")
+
 
 class Team(models.Model):
     team_code = models.CharField(default=generate_team_id, max_length=TEAM_ID_LENGTH)
