c39acc3b4bff0b719b63f9442e75083593f3d43d
AdriMM26 <adria.martinez@hackupc.com>
2025-08-04 21:10:37 +0200

    Fix teamID error

--- teams/models.py
+++ teams/models.py
@@ -1,19 +1,18 @@
 import random
-
-from django.db import models
-
+import string
+from django.db import models, IntegrityError, transaction
 from user.models import User
 
 TEAM_ID_LENGTH = 13
-
+MAX_ATTEMPTS = 33
 
 def generate_team_id():
-    s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    while True:
-        team_id = "".join(random.sample(s, TEAM_ID_LENGTH))
+    s = string.ascii_letters + string.digits  # 62 caràcters únics
+    for _ in range(MAX_ATTEMPTS):
+        team_id = "".join(random.choices(s, k=TEAM_ID_LENGTH))
         if not Team.objects.filter(team_code=team_id).exists():
             return team_id
-
+    raise Exception("No s'ha pogut generar un team_code únic després de diversos intents.")
 
 class Team(models.Model):
     team_code = models.CharField(default=generate_team_id, max_length=TEAM_ID_LENGTH)
