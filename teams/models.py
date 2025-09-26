d1a909aeb6a809ae4fca46f8ce423f69bca3ceae
enrique.andujar <enrique.andujar@estudiantat.upc.edu>
2025-09-26 19:54:48 +0200

    Changed team codes to uuids

--- teams/models.py
+++ teams/models.py
@@ -1,23 +1,24 @@
 import random
-import string
+import string, uuid
 from django.db import models
 from user.models import User
 
-TEAM_ID_LENGTH = 13
-MAX_ATTEMPTS = 33
+MAX_LENGTH_TEAM_CODE = 36
 
 
 def generate_team_id():
-    s = string.ascii_letters + string.digits  # 62 caràcters únics
-    for _ in range(MAX_ATTEMPTS):
-        team_id = "".join(random.choices(s, k=TEAM_ID_LENGTH))
-        if not Team.objects.filter(team_code=team_id).exists():
-            return team_id
-    raise Exception("No s'ha pogut generar un team_code únic després de diversos intents.")
+    return str(uuid.uuid4())
 
 
 class Team(models.Model):
-    team_code = models.CharField(default=generate_team_id, max_length=TEAM_ID_LENGTH)
+    """
+    This model represents a team of hackers. For each hacker in a team, there is one entry
+    in this table with the same team_code. The user field is a foreign key to the user
+    """
+
+    team_code = models.CharField(
+        default=generate_team_id, max_length=MAX_LENGTH_TEAM_CODE
+    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     class Meta:
